242b38f9f87dbe17f69dbe9a2180d791e3e2dd4c
Alina Souto Russo <alina.souto@alumnos.upm.es>
2026-01-03 23:57:01 +0100

    He separado el CLI en una interfaz que es la que imprime todos los System.out por lo que los he quitado en algunos comandos para que se impriman con el método printString(). Y he dejado en la interfaz CLI un comentario explicándolo mejor.

--- src/main/java/etsisi/poo/CLI.java
+++ src/main/java/etsisi/poo/CLI.java
@@ -5,7 +5,7 @@ public interface CLI {
     /*Poner el CLI como una interfaz y CLITerminal como una clase que la implementa
     permite desacoplar la lógica de la aplicación del mecanismo de entrada y salida.
     mejora la reutilización del código a parte de las dependencias y permite cambiar
-    la forma de interactuar con el usuario sin afectar al resto de la aplicación.*/
+    la forma de interactuar con el usuario sin afectar al r esto de la aplicación.*/
 
     void run();
 
