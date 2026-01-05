0f2cff334c50d3da477cfe1adc43b88dfd83cc3d
LucilaPeralta <lucila.peralta@alumnos.upm.es>
2026-01-05 19:26:04 +0000

    haciendo TicketServicePrinter

--- src/main/java/etsisi/poo/TicketServicePrinter.java
+++ src/main/java/etsisi/poo/TicketServicePrinter.java
@@ -3,9 +3,25 @@ package etsisi.poo;
 public class TicketServicePrinter implements TicketPrinter {
    @Override
     public void print(TicketModel<?> ticket){
+
+       if(ticket==null){
+           System.out.println("Ticket ID not found");
+           return;
+       }
+
+       if(ticket.getElementos().isEmpty()){
+           System.out.println("Its empty");
+           return;
+       }
+
+       System.out.println("Ticket : "+ticket.getId());
+       System.out.println("Services Included:");
+
        for (ElementoTicket<?> e : ticket.getElementos()) {
+           int cantidad =e.getQuantity();
            Service s = (Service) e.getItem();
-           // sout
+
+           System.out.printf("  %s %s x%d maxUse:%s%n", s.getId(), s.getCategory(), cantidad, s.getSmaxDate());
        }
    }
 }
