4fc388c33d6c87aeaa7de3c6a181f3e3c25ba1e7
Alina Souto Russo <alina.souto@alumnos.upm.es>
2026-01-06 21:20:55 +0100

    Casi terminada la persistencia local de ticket. Añadido el json para los tickets y el repositorio para guardar y cargar los datos

--- src/main/java/etsisi/poo/persistence/repo/Repository.java
+++ src/main/java/etsisi/poo/persistence/repo/Repository.java
@@ -4,6 +4,7 @@ public interface Repository<T> {
 
     //EXPLICACION: Un repositorio es la clase que se encarga de guardar y cargar datos,
     // pero el resto del programa no sabe si eso se guarda en JSON, base de datos, etc.
+    //
     // Esto ayuda a separar responsabilidades, si te piden guardar en XML solo cambias el repo
     // y facilita testear
 
